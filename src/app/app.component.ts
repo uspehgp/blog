a3c1f48e1c42429c2d5ca0e0c356a5a17f3bcea0
uspehgp <z995959z>
2021-03-12 16:49:33 +0300

    добавили динамику в посты

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+export interface Post {
+    title: string;
+    text: string;
+    id?: number;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+    posts: Post[] = [
+        {title: 'Хочу выучить Angular компоненты', text: 'Я все еще учу компоненты', id: 1},
+        {title: 'Следующий блок', text: 'Будет про директивы и еще про пайпы', id: 2}
+    ];
 }
