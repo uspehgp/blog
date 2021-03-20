b8c3624c797c550a7fd6d0c6195ca3d0c3d4f209
uspehgp <z995959z>
2021-03-20 15:27:26 +0300

    add ViewEncapsulation.None

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,11 +1,12 @@
-import {ChangeDetectionStrategy, Component, ContentChild, ElementRef, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ContentChild, ElementRef, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {Post} from '../app.component';
 
 @Component({
     selector: 'app-post',
     templateUrl: './post.component.html',
     styleUrls: ['./post.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    encapsulation: ViewEncapsulation.None
 })
 export class PostComponent implements OnInit {
 
