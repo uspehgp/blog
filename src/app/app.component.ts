8b64cc87357f4ada5a41b5837922da478d18f571
uspehgp <z995959z>
2021-03-20 12:24:30 +0300

    add ChangeDetectionStrategy.OnPush

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {timeout} from 'rxjs/operators';
 
 export interface Post {
     title: string;
@@ -11,7 +12,7 @@ export interface Post {
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     posts: Post[] = [
         {title: 'Хочу выучить Angular компоненты', text: 'Я все еще учу компоненты', id: 1},
         {title: 'Следующий блок', text: 'Будет про директивы и еще про пайпы', id: 2}
@@ -20,5 +21,16 @@ export class AppComponent {
     updatePosts(post: Post) {
         this.posts.unshift(post);
         console.log('Post', post);
+    }
+
+    ngOnInit(): void {
+        setTimeout(()=> {
+            console.log('Timeout')
+            this.posts[0]={
+                title: "Changed",
+                text: "Changed2",
+                id: 77
+            }
+        }, 5000)
     }
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,11 @@
-import {Component, ContentChild, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ContentChild, ElementRef, Input, OnInit} from '@angular/core';
 import {Post} from '../app.component';
 
 @Component({
     selector: 'app-post',
     templateUrl: './post.component.html',
-    styleUrls: ['./post.component.scss']
+    styleUrls: ['./post.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PostComponent implements OnInit {
 
